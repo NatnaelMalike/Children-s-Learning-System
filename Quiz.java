707598881bd6401f3ce6d3cb2b0b4a472c24667a
Tesfa <natnaelmalike@gmail.com>
2023-02-02 00:10:38 +0300

    finalizing the quiz

--- Quiz.java
+++ Quiz.java
@@ -9,36 +9,35 @@ public class Quiz extends JFrame implements ActionListener {
   JButton [] Choices = new JButton[4];
   JButton playMe = new JButton("Play~Sound");
   JButton next = new JButton("Next Question");
+  JButton startQuiz = new JButton("Start Quiz");
   Random random = new Random();
   HashMap <String, String> quizQueAns = new HashMap<>();
-  String [] questions = {"A.wav","B.wav","C.wav","D.wav","E.wav","F.wav","1.wav","2.wav","3.wav","4.wav","5.wav","6.wav"};
-  String [] answers = {"A","B","C","D","E","F","1","2","3","4","5","6"};
+  String [] questions = {"A.wav","T.wav","C.wav","D.wav","E.wav","F.wav","1.wav","2.wav","3.wav","4.wav"};
+  Character [] []options = {{'C','J','A','S'},{'H','J','T','S'},{'H','C','T','S'},{'H','J','T','D'},{'E','J','T','S'},
+  {'A','J','F','S'},{'5','2','1','0'},{'7','2','3','1'},{'5','6','8','3'},{'0','8','4','5'}};
   String [] choiceLabels = new String[4];
-  int currentQuestion = 0;
+  int currentQuestion = 1;
   int index = 0;
   int min = 0;
   int max = 3;
-  int random_int;
+  int randomIndex;
   int choiceAnswer;
   boolean choiceChecker = true;
+  int num = 0;
+  String question;
 
   public void randomChoice(){
-    String present = answers[0];
     
-    Choices[randomGenerator(3, 0)].setText(answers[0]);
-    for (int i = 0; i < Choices.length; i++) {
-      String label = String.valueOf((char)(randomGenerator(90, 65)));
-      if(Choices[i].getText() != ""){
-          continue;
-      }else {
-        if (label != present){
-          Choices[i].setText(label);
-          present = label; 
-        }
+    for (int i = 0; i < questions.length; i++) {
+      randomIndex = (int) (Math.random() * questions.length);
+       question = questions[randomIndex];
+      for (int j = 0; j < 4; j++) {
+        Choices[j].setText(String.valueOf(options[randomIndex][j]));
       }
-      
     }
+    
   }
+  
 
   public int randomGenerator(int max, int min){
     return (int)Math.floor(Math.random() * (max - min + 1) + min);
@@ -55,8 +54,11 @@ public class Quiz extends JFrame implements ActionListener {
     playMe.addActionListener(this);
     next.addActionListener(this);
     next.setBounds(150, 250,100,40 );
+    startQuiz.addActionListener(this);
+    startQuiz.setBounds(20, 300,100,40 );
     add(playMe);
     add(next);
+    add(startQuiz);
     setLayout(null);
     setSize(300,500);
     btnBoard.setLayout(btnGrid);
@@ -69,22 +71,29 @@ public class Quiz extends JFrame implements ActionListener {
   }
   @Override
   public void actionPerformed(ActionEvent e) {
-    randomChoice();
-    if (e.getSource() == playMe ) {
-      new AudioPlayer(questions[index]);
+    if(e.getSource() == startQuiz){randomChoice();}
+    
+    else if (e.getSource() == playMe ) {
+      new AudioPlayer(question);
 
     }else if(e.getSource() == next){
+      currentQuestion++;
       
       randomChoice();
-    }else if(e.getSource() == Choices){
-      
-      for (int i = 0; i < Choices.length; i++) {
-        if(Choices[i].getText() == String.valueOf(currentQuestion)){
-          JOptionPane.showMessageDialog(null, "Correct!");
-        }
-      }
-      
+    }else{
+      for(int i = 0; i<4; i++){
+        if(e.getSource() == Choices[i]){
+          if(question.charAt(0) == options[randomIndex][i]){
+            JOptionPane.showMessageDialog(null, "Correct.");
+          }else{
+          JOptionPane.showMessageDialog(null, "InCorrect.");
+          break;}
+        }     
     }
-    
+
   }
+  if (currentQuestion == questions.length) {
+    JOptionPane.showMessageDialog(null, "Quiz Completed.");
+    System.exit(0);}
+}
 }
