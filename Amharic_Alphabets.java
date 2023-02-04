5d0788c2c65fd5055d86f5c78c1e2f35b3b49237
Tesfa <natnaelmalike@gmail.com>
2023-02-04 23:45:38 +0300

    editing

--- Amharic_Alphabets.java
+++ Amharic_Alphabets.java
@@ -8,19 +8,29 @@ import java.util.HashMap;
 public class Amharic_Alphabets extends JFrame implements ActionListener {
   
   JButton[] amhButtons= new JButton[34];
-  JButton[] childButtons = new JButton[7];
-  JPanel amhPanel = new JPanel();
+  JButton[] childButtons = new JButton[7];  JPanel amhPanel = new JPanel();
   JPanel childPanel = new JPanel();
   JPanel cPanel = new JPanel();
   GridLayout amhGrid = new GridLayout(3,11);
   GridLayout childGrid = new GridLayout(1,7);
   HashMap <Object, String> amharicAlphabetsMap = new HashMap<>();
+  HashMap <Object, Integer> amharicChildAlphabetsMap = new HashMap<>();
+  HashMap <Object, String> childAudio = new HashMap<>();
   String [] fir_amh = {"ሀ","ለ","ሐ","መ","ሠ","ረ","ሰ","ሸ","ቀ","በ","ቨ","ተ","ቸ","ኀ","ነ","ኘ",
               "አ","ከ","ኸ","ወ","ዐ","ዘ","ዠ","የ","ደ","ጀ","ገ","ጠ","ጨ","ጰ","ጸ","ፀ","ፈ","ፐ"};
-  String [] ha_child = {"ሀ","ሁ","ሂ","ሃ","ሄ","ህ","ሆ"};
-  String [] le_child = {"ለ","ሉ","ሊ","ላ","ሌ","ል","ሎ"};
-  String audioName;						
+  String [][] all_Childs = {{"ሀ","ሁ","ሂ","ሃ","ሄ","ህ","ሆ"},{"ለ","ሉ","ሊ","ላ","ሌ","ል","ሎ"}};
+  String audioName;	
 
+  public void child(int parent){
+    childPanel.removeAll();
+    for (int i = 0; i < 7; i++) {
+      childButtons[i] = new JButton(all_Childs[parent][i]);
+      childPanel.add(childButtons[i]);
+      childButtons[i].setFont(new Font("Abyssinica SIL",Font.PLAIN,53));
+      childButtons[i].addActionListener(this);
+      childAudio.put(childButtons[i],"Amharic Alphabets/"+ String.valueOf(parent + 1) +"/"+ String.valueOf(i + 1) + ".wav" );
+     }
+  }
 
   public Amharic_Alphabets(){
     Font font;
@@ -39,26 +49,20 @@ public class Amharic_Alphabets extends JFrame implements ActionListener {
     childPanel.setBounds(550,250,600,100);
     cPanel.setLayout(childGrid);
     childPanel.setBounds(550,250,600,100);
-    
     add(amhPanel);
     add(childPanel);
    setLayout(null);
    setSize(1800,1000);
    
    for (int i = 0; i < amhButtons.length; i++) {
-    amhButtons[i] = new JButton(fir_amh[i]);
-    amhButtons[i].addActionListener(this);
-    amhButtons[i].setFont(new Font("Abyssinica SIL",Font.PLAIN,43));
-    amhPanel.add(amhButtons[i]);
-    amharicAlphabetsMap.put(amhButtons[i],"Amharic Alphabets/"+ String.valueOf(i + 1) + ".wav");
+        amhButtons[i] = new JButton(fir_amh[i]);
+        amhButtons[i].addActionListener(this);
+        amhButtons[i].setFont(new Font("Abyssinica SIL",Font.PLAIN,43));
+        amharicAlphabetsMap.put(amhButtons[i],"Amharic Alphabets/"+ String.valueOf(i + 1) + ".wav");
+        amharicChildAlphabetsMap.put(amhButtons[i],i);
+        amhPanel.add(amhButtons[i]);
    }
-   for (int i = 0; i < 7; i++) {
-    childButtons[i] = new JButton();
-    childPanel.add(childButtons[i]);
-    childButtons[i].setBorderPainted(false);
-    childButtons[i].setContentAreaFilled(false);
-    childButtons[i].setFont(new Font("Abyssinica SIL",Font.PLAIN,53));
-   }
+   
    setVisible(true);
   }
   public static void main(String[] args) {
@@ -69,26 +73,23 @@ public class Amharic_Alphabets extends JFrame implements ActionListener {
     for (Object key : amharicAlphabetsMap.keySet()) {
       if (e.getSource() == key) {
         audioName = amharicAlphabetsMap.get(key);
+        new AudioPlayer(audioName);
       }
     }
-    new AudioPlayer(audioName);
 
-    for (int i = 0; i < 7; i++) {
-      childButtons[i].setBorderPainted(true);
-      childButtons[i].setContentAreaFilled(true);
-    }
-   
-   if(e.getSource() == amhButtons[0]){
-      for (int i = 0; i < 7; i++) {
-        childButtons[i].setText(ha_child[i]);
+    for (Object key : amharicChildAlphabetsMap.keySet()) {
+      if (e.getSource() == key) {
+        child(amharicChildAlphabetsMap.get(key));
       }
-   }else if(e.getSource() == amhButtons[1]){
-    
-    for (int i = 0; i < 7; i++) {
-      childButtons[i].setText(le_child[i]);
-     
     }
-   }
+    
+    for (Object key : childAudio.keySet()) {
+      if(e.getSource() == key){
+        String childAudioName = childAudio.get(key);
+        new AudioPlayer(childAudioName);
+      }
+      
+    }
     
   }
 }
